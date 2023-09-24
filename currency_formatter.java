98296e3aa2dc81db17951f3fca43467fb1b0cc4e
Roshali <143102316+roshali28@users.noreply.github.com>
2023-09-24 20:39:57 +0530

    Add files via upload
    currency formatter

---
+++ currency_formatter.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class currency_formatter {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        NumberFormat n = NumberFormat.getCurrencyInstance(Locale.US);
+        String us = n.format(payment);
+        NumberFormat n1 = NumberFormat.getCurrencyInstance(new Locale("en","in"));
+        String india = n1.format(payment);
+        NumberFormat n2 = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        String china = n2.format(payment);
+        NumberFormat n3 = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        String france = n3.format(payment);
+        
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
